cf388186225c22fa0280b6be31cf7ee2802e4101
Sunzidabeg <50387582+Sunzidabeg@users.noreply.github.com>
2019-06-15 19:23:10 -0500

    Add files via upload

---
+++ Mybio.java
@@ -0,0 +1,6 @@
+public class Mybio {
+    public static void main(String[] args) {
+        System.out.println("My name is Sunzida Beg\nI am 45 years old\nMy hometown is Richardson,Texas");
+    }
+}
+
